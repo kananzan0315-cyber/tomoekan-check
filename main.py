bd61668b37d4459f5f3a9eef461ce6a8576d3d67
kananzan0315-cyber <kananzan0315@gmail.com>
2026-05-11 00:28:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello")
+import requests
+
+url = "https://tomoekan.com/8tomoekan-calender/"
+
+text = requests.get(url).text
+
+if "八ﾄﾓ4名" in text and "〇" in text:
+    print("空きあり！")
+else:
+    print("空きなし")

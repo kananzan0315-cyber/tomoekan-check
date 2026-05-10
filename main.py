74ff940f31a3d61e28747ff9df5c105658f366f1
kananzan0315-cyber <kananzan0315@gmail.com>
2026-05-11 00:49:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import requests
 
 url = "https://tomoekan.com/8tomoekan-calender/"
@@ -5,6 +6,32 @@ url = "https://tomoekan.com/8tomoekan-calender/"
 text = requests.get(url).text
 
 if "八ﾄﾓ4名" in text and "〇" in text:
-    print("空きあり！")
+
+    line_token = os.environ["LINE_TOKEN"]
+    user_id = os.environ["USER_ID"]
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {line_token}"
+    }
+
+    data = {
+        "to": user_id,
+        "messages": [
+            {
+                "type": "text",
+                "text": "トモエ館に空きあり！"
+            }
+        ]
+    }
+
+    requests.post(
+        "https://api.line.me/v2/bot/message/push",
+        headers=headers,
+        json=data
+    )
+
+    print("LINE送信！")
+
 else:
     print("空きなし")
